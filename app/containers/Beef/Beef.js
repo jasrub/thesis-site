8848290e8e579d2fb8ad91c6231ffb22ca74580d
Travis Rich <travis.s.rich@gmail.com>
2017-02-24 19:31:23 -0500

    Use include again

--- app/containers/Beef/Beef.js
+++ app/containers/Beef/Beef.js
@@ -61,7 +61,7 @@ export const Beef = React.createClass({
 		return fetch(url, {
 			method: 'POST',
 			body: form,
-			credentials: 'same-origin',
+			credentials: 'include',
 		})
 		.then((response)=> {
 			if (!response.ok) { return response.json().then(err => { throw err; }); }
