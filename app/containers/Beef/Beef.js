4d6dd38e90f4a9a8ae629b2d27b229fc5c6e78d0
Travis Rich <travis.s.rich@gmail.com>
2017-02-24 19:02:35 -0500

    Add include

--- app/containers/Beef/Beef.js
+++ app/containers/Beef/Beef.js
@@ -61,6 +61,7 @@ export const Beef = React.createClass({
 		return fetch(url, {
 			method: 'POST',
 			body: form,
+			credentials: 'include',
 		})
 		.then((response)=> {
 			if (!response.ok) { return response.json().then(err => { throw err; }); }
