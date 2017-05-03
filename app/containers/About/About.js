6bf67f780b53231ebd17b1d4a9a0e00235babd08
Jasmin Rubinovitz <jasrub@gmail.com>
2017-05-03 10:52:40 -0400

    update about

--- app/containers/About/About.js
+++ app/containers/About/About.js
@@ -11,19 +11,15 @@ export const About = React.createClass({
                 <div style={styles.container}>
                     <div style={styles.content}>
                     <p>
-                    Panorama is an interactive interface , that is used both as an open-box news aggregator, as well as a set of  news annotators, and is  based on underlying machine learning models that learn from the user as the user herself explores the material.
-                    Facebook is a black box aggregator. It selects and prioritizes posts based on rules you don't know and can't control. Christian Sandvig proposes glass-box algorithms - you can see how they work and test them, but not alter them. An open box aggregator is one where you can flip the switches, try different settings and compare their results.
+                        In the era of information, we are free to access as many news stories as we want. However, today news is so abundant that people don’t have the time to read or watch all of it, nor the time to select what stories they want to know about. We trust editors and algorithms to decide for us, giving away our control and sometimes missing the big picture.
+                        For computers, news stories are usually not annotated or categorized, they come in as an unstructured text in natural language that for machines is hard to generalize.
                     </p>
-                    <p>
-                    Panorama is built to allow open, transparent and collaborative exploration of news from all across the political map, it presents different perspectives and encourages serendipity in news exploration, vs. getting all of our news from one single source.
-                    </p>
-                    <p>
-                    Panorama is a Human-in-the-loop interface. Initially, it uses machine learning algorithms to decide  what topics each story is talking about and if the stories are positive, subjective, trending, etc.
-                    Thinking of how machine learning works in general — it pours over massive datasets and learns to generalize in smart ways, but not in the same smart way that humans generalize. As a result, it can be brilliant and also get very confused. In our case, some of the training data was a large open set of movie reviews, and while this is a great dataset to start with, it is not mapped so well to news stories.
-                    As humans interact with Panorama, they are encouraged to give better annotations to each story they read. Those labels are fed back into the algorithm to make it better.
-                    </p>
-                    <p>
-                    Panorama uses principles of information visualization and user experience  to address the problem of information overload. It allows readers to explore underlying patterns of news stories, reflect on our world by adding meaning and help manage information overload and focus the attention to what matters.
+                        <p>
+                        Panorama proposes a novel interaction method of coupling principles of data visualization and user experience with an interactive machine learning approach, easing our understanding and exploration of mass information as well as collecting nuanced annotations for the same information. This is a human machine collaboration where the computer analyzes and renders the data, making it graspable, and the user in turn gives annotated labels that help the computer better analyze the next data points it sees.
+                        </p>
+                        <p>
+                        Panorama is an interface for open, transparent and collaborative exploration of news. It addresses information overload, by allowing users to filter, organize and control their news feed. Panorama is also an interactive machine learning system, it is based on machine learning models that analyze news stories. As the user reads and explores the news, she is encouraged to submit feedback that is sent back to the underlying machine learning models, helping them improve.
+                        By showing all the available information, from all U.S mainstream media sources, and placing the power to filter in the user’s hands, Panorama encourages serendipity in news consumption and bursts filter bubbles.ciples of information visualization and user experience  to address the problem of information overload. It allows readers to explore underlying patterns of news stories, reflect on our world by adding meaning and help manage information overload and focus the attention to what matters.
                     </p>
                         <p>
                         This is a first prototype built by <a href="http://www.jasrub.com/">Jasmin Rubinovitz</a>, as part of her masters thesis at the Viral Communications group at MIT Media Lab
